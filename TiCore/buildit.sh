09844e063d5908acc2ccb54aa343043b71affc3d
Stephen Tramer <sptramer@gmail.com>
2011-12-20 10:55:44 -0800

    [TIMOB-6788] Update the JSCore project to compile correctly against iOS 5 SDK.

--- TiCore/buildit.sh
+++ TiCore/buildit.sh
@@ -4,4 +4,4 @@ xcodebuild -sdk iphoneos -configuration Release -target iOSTiCore clean
 xcodebuild -sdk iphonesimulator -configuration Release -target iOSTiCore
 xcodebuild -sdk iphoneos -configuration Release -target iOSTiCore
 
-python lipoit.py
+lipo build/Release-iphonesimulator/libiOSTiCore.a build/Release-iphoneos/libiOSTiCore.a -create -output build/libTiCore.a
--- TiCore/lipoit.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#
-#
-# make a combined library
-#
-
-import os,sys
-
-os.system("lipo build/Release-iphonesimulator/libiOSTiCore.a build/Release-iphoneos/libiOSTiCore.a -create -output build/libTiCore.a")
