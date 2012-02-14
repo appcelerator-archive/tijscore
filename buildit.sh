3428c87e8f30ad1d213445f02aac44c78253834e
Stephen Tramer <stramer@appcelerator.com>
2012-02-13 16:36:02 -0800

    [TIMOB-6292] Removed project generator and fixed gzip on buildit

--- buildit.sh
+++ buildit.sh
@@ -11,14 +11,14 @@ if (( $# > 1 )); then
     VERSION=$2
 fi
 
-xcodebuild -project TiCore/TiCore.xcodeproj -sdk iphonesimulator -configuration ${CONFIG} -target iOSTiCore clean
-xcodebuild -project TiCore/TiCore.xcodeproj -sdk iphoneos -configuration ${CONFIG} -target iOSTiCore clean
+xcodebuild -project TiCore/TiCore.xcodeproj -sdk iphonesimulator -configuration ${CONFIG} -target TiCore clean
+xcodebuild -project TiCore/TiCore.xcodeproj -sdk iphoneos -configuration ${CONFIG} -target TiCore clean
 
-xcodebuild -project TiCore/TiCore.xcodeproj -sdk iphonesimulator -configuration ${CONFIG} -target iOSTiCore
-xcodebuild -project TiCore/TiCore.xcodeproj -sdk iphoneos -configuration ${CONFIG} -target iOSTiCore
+xcodebuild -project TiCore/TiCore.xcodeproj -sdk iphonesimulator -configuration ${CONFIG} -target TiCore
+xcodebuild -project TiCore/TiCore.xcodeproj -sdk iphoneos -configuration ${CONFIG} -target TiCore
 
-lipo TiCore/build/${CONFIG}-iphonesimulator/libiOSTiCore.a TiCore/build/${CONFIG}-iphoneos/libiOSTiCore.a -create -output TiCore/build/libTiCore.a
-if [[ ! -n "$VERSION" ]]; then
+lipo TiCore/build/${CONFIG}-iphonesimulator/libTiCore.a TiCore/build/${CONFIG}-iphoneos/libTiCore.a -create -output TiCore/build/libTiCore.a
+if [[ ! -z "$VERSION" ]]; then
     # Package the ticore into a new version
     gzip -c TiCore/build/libTiCore.a > libTiCore-$VERSION.a.gz
 fi
