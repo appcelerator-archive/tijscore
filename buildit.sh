0891e709684b21ba5c2549065d1c8eed10715186
Stephen Tramer <stramer@appcelerator.com>
2012-02-14 13:13:51 -0800

    [TIMOB-6292] Added sanity check to buildit

--- buildit.sh
+++ buildit.sh
@@ -18,6 +18,15 @@ xcodebuild -project TiCore/TiCore.xcodeproj -sdk iphonesimulator -configuration 
 xcodebuild -project TiCore/TiCore.xcodeproj -sdk iphoneos -configuration ${CONFIG} -target TiCore
 
 lipo TiCore/build/${CONFIG}-iphonesimulator/libTiCore.a TiCore/build/${CONFIG}-iphoneos/libTiCore.a -create -output TiCore/build/libTiCore.a
+
+for arch in armv6 armv7 i386; do
+	SYMCOUNT=`nm TiCore/build/libTiCore.a 2>/dev/null | grep $arch | wc -l`
+	if (( $SYMCOUNT == 0 )); then
+		echo "ERROR: YOU DID NOT BUILD IN SYMBOLS FOR $arch"
+		exit 1
+	fi
+done
+
 if [[ ! -z "$VERSION" ]]; then
     # Package the ticore into a new version
     gzip -c TiCore/build/libTiCore.a > libTiCore-$VERSION.a.gz
