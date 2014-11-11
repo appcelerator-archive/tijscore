9ec69650580f32d6dee0093abd3b6717fd5efb7f
vishalduggal <vduggal@appcelerator.com>
2014-11-10 21:03:16 -0800

    Delete Old TiCore project

--- TiCore/gyp/generate-derived-sources.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p "${BUILT_PRODUCTS_DIR}/DerivedSources/TiCore/docs"
-cd "${BUILT_PRODUCTS_DIR}/DerivedSources/TiCore"
-
-/bin/ln -sfh "${SRCROOT}/.." TiCore
-export TiCore="TiCore"
-
-make --no-builtin-rules -f "TiCore/DerivedSources.make" -j `/usr/sbin/sysctl -n hw.ncpu`
