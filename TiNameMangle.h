8bd15bebe52391619d4bc758b95df922753a408f
jonalter <jonalter+github@gmail.com>
2015-01-28 16:34:03 -0800

    [TIMOB-18208] Adding name mangling header to pass AppStore Validation

---
+++ TiNameMangle.h
@@ -0,0 +1,17 @@
+/**
+ * Appcelerator Titanium Mobile
+ *
+ * Copyright (c) 2015 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License.
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+#ifndef TiNameMangle_h
+#define TiNameMangle_h
+
+#define WTFCrash Ti_WTFCrash
+#define WTFLogAlways Ti_WTFLogAlways
+#define WTFReportFatalError Ti_WTFReportFatalError
+#define WebCoreWebThreadIsLockedOrDisabled Ti_WebCoreWebThreadIsLockedOrDisabled
+
+#endif
